b753d120a8d95d089f01a6d48ee94c243f18940b
Ashwin Hegde <ashwin.hegde12@gmail.com>
2018-04-27 17:57:22 +0530

    Extract prop-types

--- src/react-trumbowyg.js
+++ src/react-trumbowyg.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import trumbowyg from 'trumbowyg'
 import svgIcons from 'trumbowyg/dist/ui/icons.svg'
 const trumbowygIconsId = 'trumbowyg-icons'
